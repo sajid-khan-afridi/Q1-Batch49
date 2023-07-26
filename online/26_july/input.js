c412e1ab2678c33d82538c3dd0e572d224ae194c
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-07-26 23:00:58 +0500

    online session 26-july

---
+++ online/26_july/input.js
@@ -0,0 +1,8 @@
+// let myName=prompt("Enter your name");
+// alert(myName?.toUpperCase());
+var str1 = 'Laurence';
+var str2 = "Svekis";
+var val1 = undefined;
+var val2 = null;
+var myNum = 1000;
+console.log(typeof (str1));
---
+++ online/26_july/input.ts
@@ -0,0 +1,10 @@
+// let myName=prompt("Enter your name");
+// alert(myName?.toUpperCase());
+
+
+let str1 = 'Laurence';
+let str2 = "Svekis";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1000;
+console.log(typeof(str1))
