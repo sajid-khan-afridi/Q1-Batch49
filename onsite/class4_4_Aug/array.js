a90c879b99d52c9f2da9a3c6b7b2d600db720672
Sajid Khan Afridi <uetianafridi@gmail.com>
2023-08-05 04:40:24 +0500

    onsite class code

---
+++ onsite/class4_4_Aug/array.js
@@ -0,0 +1,32 @@
+"use strict";
+// let arr=["one", "two","three","four"];
+// // let arr2=[1,"Ali",true];
+// arr[3]="five";
+// arr[4]="six";
+// arr[5]="seven";
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.push("four");
+// arr.shift()
+// arr.unshift("1")
+// // console.log(arr.pop())
+// let returnValue = arr.unshift("five","six");
+// console.log(returnValue);
+// console.log(arr);
+// console.log(arr.shift());
+// var pets = [];
+// pets[0] = "dog"; // adds “dog” to an array at 0 index
+// pets[1] = "cat"; // adds “cat” to an array at index 1
+// console.log(pets.shift());
+// let fruits = ["Banana", "Orange", "Apple", "Mango",2];
+// fruits.splice(2,0, 2, 3); 
+// console.log(fruits);
+// let num=[1,2,"Ali"]
+// num.splice(1,0,2,4);
+// let anyValus:(number|string|boo)
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 3); // [Orange,Lemon,Apple,Mango]
+fruits.splice(3, 2);
+console.log(fruits);
+// console.log(citrus)
